a6ba1750dbd69f055b1db60a5ce1bd616cf5608e
jerryang76 <jerryang76@yahoo.com.tw>
2022-07-01 23:04:08 +0800

    Update constant.js

--- src/constant.js
+++ src/constant.js
@@ -3,9 +3,9 @@ import Image2 from './assets/images/2.jpg';
 import Image3 from './assets/images/3.jpg';
 
 export const fonts = {
-  PMingLiU: '新細明體',
-  cwTeXKai: '楷體',
   cwTeXYen: '圓體',
+  cwTeXKai: '楷體',
+  PMingLiU: '新細明體',
   cwTeXFangSong: '仿宋體',
   Roboto: 'Roboto',
   'Noto Sans HK': 'Noto Sans HK',
@@ -13,7 +13,7 @@ export const fonts = {
 export const backgroundImages = [Image1, Image2, Image3];
 export const elderlyWords = [
   '早安',
-  '認同請分享',
-  '您知道嗎？',
-  '好人一生平安',
+  '懷寧浸信會',
+  '喜樂平安',
+  '阿們',
 ];
